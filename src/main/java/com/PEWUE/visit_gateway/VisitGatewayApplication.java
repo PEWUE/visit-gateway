0ac4e24611b4500dd856f48bc427e5dcadff6fa3
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-27 23:06:27 +0100

    initial commit

---
+++ src/main/java/com/PEWUE/visit_gateway/VisitGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.PEWUE.visit_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PEWUE/visit_gateway/VisitGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PEWUE.visit_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
